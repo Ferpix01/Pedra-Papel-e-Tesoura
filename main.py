248c94cdaadddb0d1012d57ecbe1bba9da37e77a
Felipe Caldeira <146666806+Ferpix01@users.noreply.github.com>
2023-11-26 15:47:03 -0300

    Script PPT.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import random
 
-# Vai tomar no cu piranhaaaaaa
-
 def get_user_choice():
-    print("Escolha: \n1 - Pedra \n2 - Papel \n3 - Tesoura\n4 - Mão de Deus")
+    print("Escolha: \n1 - Pedra \n2 - Papel \n3 - Tesoura")
     choice = input("O que tu quer: ")
-    while choice not in ['1', '2', '3', '4']:
-        print("Opção inválida filho da puta!")
+    while choice not in ['1', '2', '3']:
+        print("Opção inválida!")
         choice = input("O que tu quer: ")
     return choice
 
 def get_computer_choice():
-    choices = ['Pedra', 'Papel', 'Tesoura', 'Mão de Deus']
+    choices = ['Pedra', 'Papel', 'Tesoura']
     return random.choice(choices)
 
 def get_result(user_choice, computer_choice):
@@ -22,7 +20,6 @@ def get_result(user_choice, computer_choice):
         return "Empate"
     elif ((user_choice == 'Pedra' and computer_choice == 'Tesoura') or
           (user_choice == 'Tesoura' and computer_choice == 'Papel') or
-          (user_choice == 'Mão de Deus' and computer_choice == 'Papel', 'Tesoura', 'Pedra') or
           (user_choice == 'Papel' and computer_choice == 'Pedra')):
         return "Você ganhou!"
     else:
@@ -32,8 +29,7 @@ def get_choice_name(choice):
     choices = {
         '1': 'Pedra',
         '2': 'Papel',
-        '3': 'Tesoura',
-        '4': 'Mão de Deus'
+        '3': 'Tesoura'
     }
     return choices[choice]
 
@@ -47,5 +43,5 @@ while True:
     play_game()
     next_game = input("Mais um? (s/n): ")
     if next_game.lower() != 's':
-        print("Perdedor filho da puta do caralho hein")
+        print("Perdedor XD")
         break
